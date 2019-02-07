c59c68db87a5a0d72607ae071d741a51bdfccb5d
daniel Verdu <verdudaniel7@gmail.com>
2019-02-07 17:06:44 +0100

    creando un componente funcional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Learning React</h1>
+        <Person/>
       </div>
     );
     /* return(
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+//importamos React
+import React from "react";
+
+const person = () => {
+    return <p>I´m a Person</p>
+}
+
+// exportamos person
+export default person;
