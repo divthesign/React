36dcb612bca6f77c7813445666952557ac95d0e4
daniel Verdu <verdudaniel7@gmail.com>
2019-02-07 18:02:00 +0100

    entendiendo state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person.js'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Learning React</h1>
-        <Person name="Max" age="25" />
-        <Person name="Manu" age="29"> And I like swimming </Person>
-        <Person name="Stephanie" age="26" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //React se podría escribir así pero no es JSX
